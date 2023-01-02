ea87198d78ab03fbbb14b328abb943a42f1b016d
Jaedson Silva <contacanal798@gmail.com>
2023-01-01 21:47:41 -0300

    fix: change key_sum variable order

--- secpickle/secpickle.py
+++ secpickle/secpickle.py
@@ -11,8 +11,8 @@ def _verify(data: bytes, key: str) -> bool:
     original_hash = data[:64]
     obj = data[64:]
 
+    obj_hash = hashlib.sha256(obj).hexdigest()
     key_sum = (key + obj_hash).encode()
-    obj_hash = hashlib.sha256(obj)
     check_hash = hashlib.sha256(key_sum)
 
     return original_hash == check_hash
