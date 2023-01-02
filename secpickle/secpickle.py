41bab6d177b40e6ba9d32251dbc442001b8734e3
Jaedson Silva <contacanal798@gmail.com>
2023-01-01 21:40:26 -0300

    feat: create load() method to unpack file data

--- secpickle/secpickle.py
+++ secpickle/secpickle.py
@@ -1,4 +1,10 @@
 import hashlib
+import pickle
+import io
+
+from typing import Any
+
+from . import exceptions
 
 
 def _verify(data: bytes, key: str) -> bool:
@@ -10,3 +16,14 @@ def _verify(data: bytes, key: str) -> bool:
     check_hash = hashlib.sha256(key_sum)
 
     return original_hash == check_hash
+
+
+def load(file: io.BufferedReader, key: str) -> Any:
+    data = file.read()
+
+    if _verify(data, key):
+        obj = data[64:]
+        unpickle_obj = pickle.loads(obj)
+        return unpickle_obj
+    else:
+        raise exceptions.IntegrityUnconfirmedError('Unable to confirm file integrity')
